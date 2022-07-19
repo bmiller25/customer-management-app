6630bcd4bdfcf4e2ad96a737275423556204d044
Benjamin J. Miller <benjaminjmiller217@yahoo.com>
2022-07-19 16:06:46 -0700

    Added SQL to create table

---
+++ create-customer-table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE customer (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(45),
+  last_name VARCHAR(45),
+  gender VARCHAR(1),
+  address VARCHAR(150),
+  phone_number VARCHAR(15),
+  name_on_credit_card VARCHAR(45),
+  credit_card_number VARCHAR(20),
+  expiration_date VARCHAR(4),
+  csc VARCHAR(4),
+  rewards_member TINYINT,
+  rewards_points INT
+);
